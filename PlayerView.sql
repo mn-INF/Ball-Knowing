440e4e4085fcf3313bb0972eb881a1a8baf80ba3
mn-INF <164985907+mn-INF@users.noreply.github.com>
2025-05-23 02:20:21 -0400

    Update and rename PlayerView.sql to BasicPlayerView.sql

--- PlayerView.sql
+++
@@ -1,6 +0,0 @@
-SELECT season_id, description, first_name, last_name, full_name
-FROM player_season_stat_totals as psst
-JOIN players as pl ON psst.player_id = pl.player_id
-JOIN season_types as st ON psst.season_type_id = st.season_type_id
-JOIN teams on psst.team_id = teams.team_id
-ORDER BY season_id ASC, description DESC, full_name ASC;
